328503e3b75409df75a77bd53aef912438de6971
Nathan Kerr <nathan@nathankerr.com>
2014-05-25 03:30:51 +0200

    file: limit Objects to the objects defined in this package.
    Added thisIsABasicPDFObject() to the Object interface which, as an
    unexported function can only be implemented in the file package to
    limit the interface matchs for Object to just those object types which
    can actually be handled.

--- file/objects.go
+++ file/objects.go
@@ -15,6 +15,7 @@ import (
 // - Stream §7.3.8
 // - Null §7.3.9
 type Object interface {
+	thisIsABasicPDFObject()
 	io.WriterTo
 }
 
@@ -22,31 +23,45 @@ type Object interface {
 // - §7.3.2
 type Boolean bool
 
+func (Boolean) thisIsABasicPDFObject() {}
+
 // Integer objects represent mathematical integers.
 // - §7.3.3
 type Integer int
+
+func (Integer) thisIsABasicPDFObject() {}
 
 // Real objects represent mathematical real numbers.
 // - §7.3.3
 type Real float64
 
+func (Real) thisIsABasicPDFObject() {}
+
 // A String object consists of zero or more bytes.
 // - §7.3.4
 type String []byte
+
+func (String) thisIsABasicPDFObject() {}
 
 // A Name object is an atomic symbol uniquely defined by a sequence of
 // any characters (8-bit values) except null (character code 0)
 // - §7.3.5
 type Name string
 
+func (Name) thisIsABasicPDFObject() {}
+
 // An Array object is a one-dimensional collection of objects
 // arranged sequentially.
 // - §7.3.6
 type Array []Object
 
+func (Array) thisIsABasicPDFObject() {}
+
 // A Dictionary object is an associative table mapping Names to Objects.
 // - §7.3.7
 type Dictionary map[Name]Object
+
+func (Dictionary) thisIsABasicPDFObject() {}
 
 // A Stream object is a sequence of bytes.
 // - §7.3.8
@@ -55,9 +70,13 @@ type Stream struct {
 	Stream []byte
 }
 
+func (Stream) thisIsABasicPDFObject() {}
+
 // The Null object has a type and value that are unequal to any other object.
 // - §7.3.9
 type Null struct{}
+
+func (Null) thisIsABasicPDFObject() {}
 
 // An ObjectReference references a specific Object with the exact
 // ObjectNumber and GenerationNumbers specified.
@@ -67,6 +86,8 @@ type ObjectReference struct {
 	GenerationNumber uint64 // non-negative integer
 }
 
+func (ObjectReference) thisIsABasicPDFObject() {}
+
 // An IndirectObject gives an Object an ObjectReference by which
 // other Objects can refer to it.
 // - §7.3.10
@@ -74,3 +95,5 @@ type IndirectObject struct {
 	ObjectReference
 	Object
 }
+
+func (IndirectObject) thisIsABasicPDFObject() {}
