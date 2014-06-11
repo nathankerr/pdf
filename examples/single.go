40e2d2f489e32dc4c54126c577e8af2ff0a55a7d
Nathan Kerr <nathan@nathankerr.com>
2014-06-11 07:17:53 +0200

    single: able to reduce to first page

---
+++ examples/single.go
@@ -0,0 +1,132 @@
+package main
+
+// in pdf.change to single page view of all pages
+// - place all pages onto single page
+// - impose properly
+
+import (
+	"github.com/juju/errgo"
+	pdf "github.com/nathankerr/pdf/file"
+	"log"
+	"os"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: single [pdf.pdf]")
+	}
+
+	filename := os.Args[1]
+
+	file, err := pdf.Open(filename)
+	if err != nil {
+		log.Fatalln(errgo.Details(err))
+	}
+
+	catalog := file.Get(file.Root).(pdf.Dictionary)
+	pages := getPages(file, catalog[pdf.Name("Pages")].(pdf.ObjectReference))
+
+	// single_page := pdf.Dictionary{
+	// 	pdf.Name("Type"): pdf.Name("Page"),
+	// }
+
+	log.Println(pages[0])
+
+	log.Println("parent:", file.Get(pages[0].Object.(pdf.Dictionary)[pdf.Name("Parent")].(pdf.ObjectReference)))
+
+	single_page := pages[0].Object.(pdf.Dictionary)
+	delete(single_page, pdf.Name("Parent"))
+
+	// rebuild the pdf structure
+	page_list := file.Get(catalog[pdf.Name("Pages")].(pdf.ObjectReference)).(pdf.Dictionary)
+	page_list[pdf.Name("Kids")] = pdf.Array{}
+	page_list_ref, err := file.Add(page_list)
+
+	single_page[pdf.Name("Parent")] = page_list_ref
+	single_page_ref, err := file.Add(single_page)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	page_list[pdf.Name("Kids")] = append(page_list[pdf.Name("Kids")].(pdf.Array), single_page_ref)
+
+	log.Println(single_page_ref)
+
+	// outlines
+	outlines := pdf.Dictionary{
+		pdf.Name("Type"):  pdf.Name("Outlines"),
+		pdf.Name("Count"): pdf.Integer(0),
+	}
+	outlines_ref, err := file.Add(outlines)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// update page list count
+	page_list[pdf.Name("Count")] = pdf.Integer(len(page_list[pdf.Name("Kids")].(pdf.Array)))
+	file.Add(pdf.IndirectObject{
+		ObjectReference: page_list_ref,
+		Object:          page_list,
+	})
+
+	// log.Println(file.Get(page_list[pdf.Name("Parent")].(pdf.ObjectReference)))
+
+	// try with a new catalog
+	// new_catalog := pdf.Dictionary{
+	// 	pdf.Name("Type"):     pdf.Name("Catalog"),
+	// 	pdf.Name("Pages"):    page_list_ref,
+	// 	pdf.Name("Outlines"): outlines_ref,
+	// }
+	// new_catalog_ref, err := file.Add(new_catalog)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// file.Root = new_catalog_ref
+
+	// try with an updated old catalog
+	catalog[pdf.Name("Pages")] = page_list_ref
+	catalog[pdf.Name("Outlines")] = outlines_ref
+	new_catalog_ref, err := file.Add(catalog)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	file.Root = new_catalog_ref
+
+	err = file.SaveAs("copy-" + filename)
+	if err != nil {
+		log.Fatalln(errgo.Details(err))
+	}
+
+	err = file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// transforms the page tree from the file into an array of pages
+func getPages(file *pdf.File, ref pdf.ObjectReference) []pdf.IndirectObject {
+	// log.Println("getPages:", ref)
+	pages := []pdf.IndirectObject{}
+
+	page_node := file.Get(ref).(pdf.Dictionary)
+
+	switch page_node[pdf.Name("Type")] {
+	case pdf.Name("Pages"):
+		for _, kid_ref := range page_node[pdf.Name("Kids")].(pdf.Array) {
+			kid_pages := getPages(file, kid_ref.(pdf.ObjectReference))
+			pages = append(pages, kid_pages...)
+			// kid := file.Get(kid_ref.(pdf.ObjectReference)).(pdf.Dictionary)
+			// log.Println(kid)
+		}
+	case pdf.Name("Page"):
+		pages = append(pages, pdf.IndirectObject{
+			ObjectReference: ref,
+			Object:          page_node,
+		})
+	default:
+		panic(string(page_node[pdf.Name("Type")].(pdf.Name)))
+	}
+
+	return pages
+}
